49eff95bb7c51673a2a5b32c5ac52b1ff1cfb2af
Sam Aung <92190177+aung-st@users.noreply.github.com>
2023-11-02 17:58:02 +0000

    Add: display menu items function to clean up event listener code

--- restaurant-menu/app.js
+++ restaurant-menu/app.js
@@ -51,8 +51,13 @@ const menu = [
 ];
 
 const sectionCenter = document.querySelector(".section-center");
+
 window.addEventListener("DOMContentLoaded",function(){
-    let displayMenu = menu.map(function(item){
+    displayMenuItems(menu);
+});
+
+function displayMenuItems(menuItems){
+    let displayMenu = menuItems.map(function (item){
         
         return `<article class="menu-item">
                 <img src=${item.img} class="photo"
@@ -70,4 +75,4 @@ window.addEventListener("DOMContentLoaded",function(){
     });
     displayMenu = displayMenu.join("");
     sectionCenter.innerHTML = displayMenu;
-});
+};
