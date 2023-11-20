24e3e9d6bd513a55ad490a67e7c568b022b6e343
Sam Aung <92190177+aung-st@users.noreply.github.com>
2023-11-20 21:00:08 +0000

    Add: condition to close another faq if a different faq is opened

--- frequently-asked-questions/app.js
+++ frequently-asked-questions/app.js
@@ -1,8 +1,16 @@
-const question = document.querySelectorAll(".question");
+const questions = document.querySelectorAll(".question");
 
-question.forEach(function (question){
+questions.forEach(function (question) {
     const btn = question.querySelector(".question-btn");
-    btn.addEventListener("click", function(){
+    
+    btn.addEventListener("click", function() {
+        
+        questions.forEach(function (faq) {
+            if (faq !== question) {
+                faq.classList.remove("show-text");
+            }
+        });
+
         question.classList.toggle("show-text");
     });
 });
