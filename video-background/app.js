90ee1323a160a2fb7e45e0b5c7094bf25360a8b6
Sam Aung <92190177+aung-st@users.noreply.github.com>
2023-11-21 00:15:30 +0000

    Add: event listener to show preloader until entire content is loaded

--- video-background/app.js
+++ video-background/app.js
@@ -1,6 +1,13 @@
+const preloader = document.querySelector(".preloader");
 const btn = document.querySelector(".switch-btn");
 const video = document.querySelector(".video-container");
 
+// preloader
+window.addEventListener("load",function(){
+    preloader.classList.add("hide-preloader");
+});
+
+// play/pause switch
 btn.addEventListener("click", function() {
     if (!btn.classList.contains("slide")) {
         btn.classList.add("slide");
